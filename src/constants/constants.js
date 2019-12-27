a7c4afc3135b410bae441b4d0a337cff56d63632
Lucas Dias <lucas.mendonca159@gmail.com>
2019-12-27 08:33:36 -0300

    Uploads the app's initial version

---
+++ src/constants/constants.js
@@ -0,0 +1,25 @@
+//Constants
+const navbar = {
+    title: '<i class="large material-icons" style="font-size: 100px">assignment</i>',
+    homeLink: 'Início ',
+    notesLink: 'Minhas Notas'
+}
+
+const add = {
+    textareaPlaceholder: 'Escreva sua nota aqui',
+    button: '<i class="large material-icons" style="font-size: 50px">add</i>'
+}
+
+/**
+ * Returns the navbar constants
+ * implemented by: Lucas Dias
+ * 27/12/2019
+ */
+const getNavbarConstants = () => navbar
+
+/**
+ * Returns the 'add new note' constants
+ * implemented by: Lucas Dias
+ * 27/12/2019
+ */
+const getAddConstants = () => add
