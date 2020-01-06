fb89afcf983568fcfe8b27a1285afa2cd17bee80
Lucas Dias <lucas.mendonca159@gmail.com>
2020-01-05 23:22:46 -0300

    Idk why this branch keeps causing trouble...

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,39 +1,22 @@
 //Constants
-<<<<<<< HEAD
-<<<<<<< Updated upstream
 const navbar = {
     title: '<i class="large material-icons" style="font-size: 100px">assignment</i>',
     homeLink: 'Início ',
     notesLink: 'Minhas Notas'
-=======
-=======
->>>>>>> ef0bd9ca86a6d4fba76510e0782b376837518a72
+}
 const icons = {
     navbar: {
 		TITLE: '<i class="large material-icons" style="font-size: 100px">assignment</i>'
 	},
 	notes: {
-<<<<<<< HEAD
-<<<<<<< HEAD
 		BUTTON: '<i class="large material-icons">more_vert</i>',
 		ADD: '<i class="large material-icons">add</i>'
-=======
-		BUTTON: '<i class="large material-icons">more_vert</i>'
->>>>>>> ef0bd9ca86a6d4fba76510e0782b376837518a72
-=======
-		BUTTON: '<i class="large material-icons">more_vert</i>',
-		ADD: '<i class="large material-icons">add</i>'
->>>>>>> master
 	},
 	noteMenu: {
 		RETURN: '<i class="large material-icons">keyboard_return</i>',
 		DELETE: '<i class="large material-icons">delete</i>',
 		EDIT: '<i class="large material-icons">edit</i>'
 	}
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
->>>>>>> ef0bd9ca86a6d4fba76510e0782b376837518a72
 }
 
 /**
--- src/scripts/handleNotes.js
+++ src/scripts/handleNotes.js
@@ -1,56 +1,19 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-let ButtonId = ""
-
->>>>>>> ef0bd9ca86a6d4fba76510e0782b376837518a72
-=======
->>>>>>> master
 const openNoteMenu = e => {
 	let menuId = e.id + e.id
 	let getMenu = document.getElementById(menuId)
 	getMenu.style.display = 'block'
 	setTimeout(() => { 
 		e.style.display = 'none' 
-<<<<<<< HEAD
-<<<<<<< HEAD
 	}, 800)
 	localStorage.setItem('lastButtonId', e.id)
-=======
-		setButtonId(e)
-	}, 800)
-	
->>>>>>> ef0bd9ca86a6d4fba76510e0782b376837518a72
-=======
-	}, 800)
-	localStorage.setItem('lastButtonId', e.id)
->>>>>>> master
 }
 
 const closeNoteMenu = e => {
 	let menuElement = e.parentElement
-<<<<<<< HEAD
-<<<<<<< HEAD
 	let noteButton = document.getElementById(localStorage.getItem('lastButtonId'))
-=======
-	let noteButton = ButtonId
->>>>>>> ef0bd9ca86a6d4fba76510e0782b376837518a72
-=======
-	let noteButton = document.getElementById(localStorage.getItem('lastButtonId'))
->>>>>>> master
 	menuElement.style.display = 'none'
 	noteButton.className = 'note-button w3-animate-opacity'
 	setTimeout(() => { 
 		noteButton.style.display = 'block' 
 	}, 100)
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
-const setButtonId = e => {
-	ButtonId = e
-}
->>>>>>> ef0bd9ca86a6d4fba76510e0782b376837518a72
-=======
->>>>>>> master
